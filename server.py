b1b3fede7cb855205d38c219aa225ee334bd79b2
Bret Farley <bretfarley12@mittymonarchl.com>
2017-10-14 21:02:11 -0700

    added sign in and registration

--- server.py
+++ server.py
@@ -2,13 +2,21 @@ from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 import time
 import re
+import md5
 
 app = Flask(__name__)
 app.secret_key = "waow"
-#mysql = MySQLConnector(app,'redditdb')
+mysql = MySQLConnector(app,'reddit')
 
 @app.route('/')
 def index():
+    if not "loggedOn" in session:
+        session["loggedOn"] = False
+    if not "username" in session:
+        session["username"] = " "
+    print session['username']
+    print session['loggedOn']
+    
     '''
     make a query to fetch the top rated posts from the database and pass
     them into the html page.
@@ -43,6 +51,95 @@ def register():
     '''
     return render_template('register.html')
 
+@app.route('/byeFelicia', methods = ['POST'])
+def logoff():
+    session['loggedOn'] = False
+    session['username'] = ""
+    return redirect('/')
+
+@app.route('/logAndReg', methods=['POST'])
+def logAndReg():
+    if request.form['action'] == 'logOff':
+        session['loggedOn'] = False
+        session['username'] = "null"
+        return redirect('/')
+    if request.form['action'] == 'signIn':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = md5.new(password).hexdigest()
+        check = "SELECT * FROM users"
+        
+        for i in (mysql.query_db(check)):
+            #change password to hashed_password below
+            if i['username'] == username and i['password'] == hashed_password:
+                session['loggedOn'] = True
+                session['username'] = username
+                flash("Welcome {}!".format(i['username']))
+                return redirect('/')
+        flash('incorrect username/password combo')
+        return redirect('/')
+
+        '''
+        1) make a query call to see if both the username are in the db and that they
+        belong to the same user
+        2) if not, redirect to index and set a flash message for incorrect login
+        '''
+
+    else:
+        #get data from forms
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = md5.new(password).hexdigest()
+        confirm = request.form['confirm']
+        
+
+        data = { #change password to hashed_password below
+                'username': username,
+                'email': email,
+                'password': hashed_password
+                }
+        #set all data in a dictionary
+        
+        query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)"
+        
+        properLogin = True
+
+        check = "SELECT * FROM users"
+        if len(username) < 1:
+            flash("Please enter a username")
+            properLogin = False
+        for i in (mysql.query_db(check)):
+            if i['username'] == username:
+                flash("Username already in database")
+                properLogin = False
+
+        my_re = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        check = "SELECT * FROM users"
+        for i in (mysql.query_db(check)):
+            if i['email'] == email:
+                flash("email already in database")
+                properLogin = False
+        if not my_re.match(email):
+            flash("please use a proper email")
+            properLogin = False
+        
+        if len(password) < 8:
+            flash("password must be at least 8 characters long")
+            properLogin = False
+        if password != confirm:
+            flash("passwords must match")
+            properLogin = False
+        
+        if properLogin:
+            mysql.query_db(query, data)
+            flash("Welcome to this pointless website {}!".format(username))
+            session['loggedOn'] = True
+            session['username'] = username
+            return redirect('/')
+        else:
+            return redirect('/register')
+
 @app.route('/users')
 def users():
     '''
@@ -68,45 +165,5 @@ def submit():
     '''
     return render_template('submit.html')
 
-# @app.route('/process', methods=['POST']) 
-# def create():
-#     email = request.form['email']
-#     data ={'email': email}
-#     query = "INSERT INTO emails (email, created_at) VALUES (:email, NOW())"
-#     check = "SELECT * FROM emails"
-#     print mysql.query_db(check)
-
-#     my_re = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-#     for i in (mysql.query_db(check)):
-#         if i['email'] == email:
-#             flash("email already in database")
-#             return redirect('/')
-#     if not my_re.match(email):
-#         flash("bad email")
-#     else:
-#         print "good email"
-#         mysql.query_db(query, data)
-#         return redirect('/success')
-#     return redirect('/')
-
-# @app.route('/success')
-# def success():
-#     query = "SELECT email, created_at FROM emails"
-#     emails = mysql.query_db(query)
-
-#     return render_template("success.html", emails = emails)
-
-
-
-
-
-# @app.route('/friends/<friend_id>')
-# def show(friend_id):
-#     query = "SELECT * FROM friends WHERE id = :specific_id"
-#     data = {'specific_id': friend_id}
-#     friends = mysql.query_db(query, data)
-#     return render_template('index.html', one_friend=friends[0])
-
-    
 
 app.run(debug=True)
